7f2576f77f528b37ed0674dcef6e5c4373fe1470
liznomura <lizn@hawaii.edu>
2017-07-06 11:33:04 -1000

    created shoppinglist thing

--- js/app.js
+++ js/app.js
@@ -1,8 +1,29 @@
 /*jshint esversion:6*/
 (function() {
-const myList = new ShoppingList();
-const initialList = myList.render();
-const content = document.getElementById('content');
+  const myList = new ShoppingList();
+  const initialList = myList.render();
+  const content = document.getElementById('content');
 
-content.insertAdjacentHTML('beforeend', initialList);
+  content.innerHTML = initialList;
+
+  const addButton = document.getElementById('add_shopping_list_item_button');
+
+  const title = document.getElementById('title'); // grabs input from title box
+  const description = document.getElementById('description'); // grabs input from description box
+
+
+  addButton.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    // grab inputs
+    // make new shopping list item
+    // add to shopping list item array
+    // rerender shopping list
+
+    const new_shopping_list_item = new ShoppingListItem(title.value, description.value);
+
+    myList.addItem(new_shopping_list_item);
+
+    content.innerHTML = myList.render();
+  });
 })();
