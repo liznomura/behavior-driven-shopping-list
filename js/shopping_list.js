f4ca0c8b4390b43007cf60a8d825c29662164eaf
liznomura <lizn@hawaii.edu>
2017-07-05 19:56:16 -1000

    validated addItem tests

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -3,4 +3,12 @@ class ShoppingList {
   constructor() {
     this.items = [];
   }
+
+  addItem(item) {
+    if(!(item instanceof ShoppingListItem)) {
+      throw new Error('Must be a ShoppingListItem!');
+    }
+
+    this.items.push(item);
+  }
 }
