1c3f528462ffdf7c5c6e2b11767338d1c5b8fe69
kwokbk <kwokbk@hawaii.edu>
2017-07-05 18:34:37 -1000

    shoppingListItem class, name, descriptiong, is_done

---
+++ js/shopping_list.js
@@ -0,0 +1,7 @@
+/* jshint esversion: 6 */
+class ShoppingList {
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+  }
+}
---
+++ js/shopping_list_item.js
@@ -0,0 +1,7 @@
+class ShoppingListItem {
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+    this.is_done = false;
+  }
+}
